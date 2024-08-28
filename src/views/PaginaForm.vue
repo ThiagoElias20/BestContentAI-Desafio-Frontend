02e00829ddc786e22ef73fbccc2396d652cd5943
ThiagoElias20 <contatothiagoelias22@gmail.com>
2024-08-28 15:07:31 -0300

    Adicionado o component de Input

--- src/views/PaginaForm.vue
+++ src/views/PaginaForm.vue
@@ -1,7 +1,9 @@
-<script></script>
+<script setup> 
+    import Input from '../components/Input.vue';
+</script>
 
 <template>
-    <div class="paginaInicial">
+    <div class="paginaForm">
         <main>
             <div class="seta">
                 <a href="" @click="$router.push('/')"><img src="../assets/seta.svg" alt=""></a>
@@ -12,14 +14,10 @@
                 </div>
                 <form action="">
                     <div class="inputs">
-                        <label for="">Nome da Marca</label>
-                        <input type="text" placeholder="Ex: Dr. Gabriel; Roupas Multimarcas; Pizzaria dos Sabores">
-                        <label for="">Telefone</label>
-                        <input type="text" placeholder="(00) 0 0000 - 0000" required>
-                        <label for="">Email</label>
-                        <input type="text" placeholder="xxxxxxxxxxx@xxxxx.com" required>
-                        <label for="">Com o quê você trabalha?</label>
-                        <input type="text" placeholder="Descreva sua marca/produto, seu perfil em algumas palavras, para nossa IA entender melhor o seu contexto." required>
+                        <Input inputClass="NomeMarca" inputId="nomeMarca" label="Nome da Marca" placeholder="Ex: Dr. Gabriel; Roupas Multimarcas; Pizzaria dos Sabores"/>
+                        <Input inputClass="Telefone" inputId="telefone" label="Telefone" placeholder="(00) 0 0000 - 0000"/>
+                        <Input inputClass="Email" inputId="email" label="Email" placeholder="xxxxxxxxxxx@xxxxx.com"/>
+                        <Input inputClass="Desc" inputId="desc" label="Com o quê você trabalha?" placeholder="Descreva sua marca/produto, seu perfil em algumas palavras, para nossa IA entender melhor o seu contexto."/>
                         <button type="submit">Continue</button>
                     </div>
                 </form>
@@ -29,26 +27,26 @@
 </template>
 
 <style scoped>
-    .paginaInicial {
+    .paginaForm {
         display: flex;
         justify-content: center;
         align-items: center;
         height: 97vh;
         margin: 0 15vw 0 15vw;
     } 
-    .paginaInicial main {
+    .paginaForm main {
         position: relative;
         width: 100%;
     }
-    .paginaInicial main .seta {
+    .paginaForm main .seta {
         position: absolute;
         top: 10px;
         left: 10px;
     }
-    .paginaInicial main .seta img {
+    .paginaForm main .seta img {
         height: 20px;
     }
-    .paginaInicial main .inputCard {
+    .paginaForm main .inputCard {
         display: flex;
         flex-direction: column;
         justify-content: center;
@@ -59,53 +57,28 @@
         padding: 25px;
         margin: 0 auto; /* Para centralizar o card */
     }
-    .paginaInicial main .inputCard img {
-        height: 200px;
-    }
-    .paginaInicial main .inputCard .textos {
+    .paginaForm main .inputCard .textos {
         text-align: center;
         color: #181818;
     }
-    .paginaInicial main .inputCard .textos h1 {
+    .paginaForm main .inputCard .textos h1 {
         font-size: 1.15rem;
     }
-    .paginaInicial main .inputCard .textos p {
-        font-size: 0.8rem;
-    }
-    .paginaInicial main .inputCard form {
+    .paginaForm main .inputCard form {
         width: 100%;
     }
-    .paginaInicial main .inputCard .inputs {
+    .paginaForm main .inputCard .inputs {
         display: flex;
         flex-direction: column;
         align-items: center;
         width: inherit;
     }
-    .paginaInicial main .inputCard .inputs input {
-        height: 30px;
-        width: 225px;
-        margin-bottom: 15px;
-        border-radius: 5px;
-        border: 1px solid #E0E1E6;
-        outline: none;
-        font-size: 0.75rem;
-    }
-    .paginaInicial main .inputCard .inputs input:focus {
+    .paginaForm main .inputCard .inputs input:focus {
         transition: all 0.5s ease;
         border: 1px solid #E73C7E;
         box-shadow: 0px 0px 3px #E73C7E;
     }
-    .paginaInicial main .inputCard .inputs input#instagram {
-        background: url(../assets/instagram.svg) no-repeat 7px;
-        background-size: 25px;
-        padding-left: 40px;
-    }
-    .paginaInicial main .inputCard .inputs input#globe {
-        background: url(../assets/globe.svg) no-repeat 7px;
-        background-size: 30px;
-        padding-left: 40px;
-    }
-    .paginaInicial main .inputCard .inputs button {
+    .paginaForm main .inputCard .inputs button {
         height: 40px;
         width: inherit;
         background-color: #E73C7E;
@@ -116,20 +89,12 @@
         border-radius: 50px;
         cursor: pointer;
     }
-    .paginaInicial main .inputCard .inputs button:hover {
+    .paginaForm main .inputCard .inputs button:hover {
         background-color: #FC2E7E;
         transition: all 0.3s ease;
     }
-    .paginaInicial main .inputCard .inputs button:active{
+    .paginaForm main .inputCard .inputs button:active{
         transform: translateY(2px); 
         transition: all 0.1s ease;
-    }
-    .paginaInicial main .inputCard .inputs a {
-        text-transform: uppercase;
-        text-decoration-line: underline;
-        margin-bottom: 40px;
-        font-size: 0.7rem;
-        font-weight: 600;
-        color: #86868a;
     }
 </style>
