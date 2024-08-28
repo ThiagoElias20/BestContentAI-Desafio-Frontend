ea3c2f3d9927a7ba02c0eacc0b352d7f3119e68e
ThiagoElias20 <contatothiagoelias22@gmail.com>
2024-08-27 22:33:16 -0300

    Remoção de paginas não utilizaveis e adesão da PaginaInicial com estrutura simples

---
+++ src/views/PaginaInicial.vue
@@ -0,0 +1,19 @@
+<script></script>
+
+<template>
+    <img src="../assets/logo.svg" alt="imagem">
+    <h1>Insira seu Instagram ou site e deixe a IA criar para você</h1>
+    <p>Ao criar sua marca utilizando um dos links abaixo, conseguiremos identificar sua logo, cores e outros detalhes automaticamente!</p>
+    <input type="text" placeholder="Vincular Instagram (recomendado)">
+    <input type="text" placeholder="Víncular Website">
+    <small>Não tenho rede social ou site</small>
+</template>
+
+<style scoped>
+    img {
+        height: 150px;
+    }
+    small {
+        text-transform: uppercase;
+    }
+</style>
